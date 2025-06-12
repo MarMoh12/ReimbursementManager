a1a010786fe6f1b9569b1af0999499d96f3f8748
MarMoh12 <marcel.mohrmann.mm@gmail.com>
2025-06-13 00:50:27 +0200

    Checking URL log

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -13,6 +13,8 @@ const LoginPage: React.FC = () => {
     e.preventDefault();
     setLoading(true);
 
+    console.log(`${process.env.REACT_APP_API_BASE_URL}/api/token/`)
+
     const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/token/`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
